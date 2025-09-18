ddb8dab32169d9ca85b3257dc6a88c20b4445af2
Kolin-ko <kkolinn4457@gmail.com>
2025-09-18 18:41:45 +0700

    Auth

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -5,30 +5,71 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
-    public function login(LoginRequest $request){
+    public function login(LoginRequest $request)
+    {
 
         try{
+            $validated = $request->safe()->all();
+
+            if (!Auth::attempt($validated)){
+                return response()->json([
+                    'message' => 'Email atay password salah',
+                    'data' => null
+                ], 401);
+            }
+
+            $user = $request->user();
+            $token = $user->createToken('laravel_api', ['*'])->plainTextToken;
+
+            return response()->json([
+                'message' => 'login Berhasil',
+                'user' => $user,
+                'token' => $token
+            ], 200);
 
         } catch (Exception $e){
-            //
+             return response()->json([
+                    'message' => $e->getMessage(),
+                    'data' => null
+                ], 500);
         }
     }
 
-    public function register(RegisterRequest $request){
-
+    public function register(RegisterRequest $request)
+    {
         try{
-            $validated = $request -> $safe() -> $all();
+            $validated = $request->safe()->all();
+
+            $passwordHash = Hash::make($validated['password']);
+
+            $validated['password'] = $passwordHash;
+
+            $response = User::create($validated);
+
+            if($response){
+                return response()->json([
+                    'message' => 'register berhasil',
+                    'data' => null
+                ], 201);
+            }
         } catch (Exception $e){
-            
+            return response()->json([
+                    'message' => $e->getMessage(),
+                    'data' => null
+                ], 201);
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
 
         try{
 
